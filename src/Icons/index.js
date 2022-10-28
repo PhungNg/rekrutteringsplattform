57d4b7e083fcf9b875171585993e7d37f3bd5ed6
Phung Nguyen <44967773+PhungNg@users.noreply.github.com>
2022-10-28 14:49:56 +0200

    Add icons

---
+++ src/Icons/index.js
@@ -0,0 +1,27 @@
+import calendar from './calendar.png'
+import chain from './chain.png'
+import clock from './clock.png'
+import flag from './flag.png'
+import home from './home.png'
+import key from './key.png'
+import mail from './mail.png'
+import phone from './phone.png'
+import plus from './plus.png'
+import profile from './profile.png'
+import rocket from './rocket.png'
+import upload from './upload.png'
+
+export {
+    calendar,
+    chain,
+    clock,
+    flag,
+    home,
+    key,
+    mail,
+    phone,
+    plus,
+    profile,
+    rocket,
+    upload
+}
