19327e7de26ce9b23d8260e217dd280ce0de72ea
Phung Nguyen <44967773+PhungNg@users.noreply.github.com>
2022-11-01 16:04:24 +0100

    Add icons

--- src/Icons/index.js
+++ src/Icons/index.js
@@ -1,26 +1,40 @@
+import accordionDown from './accordion-down.png'
+import accordionUp from './accordion-up.png'
+import bin from './bin.png'
 import calendar from './calendar.png'
 import chain from './chain.png'
 import clock from './clock.png'
+import edit from './edit.png'
+import file from './file.png'
 import flag from './flag.png'
 import home from './home.png'
 import key from './key.png'
 import mail from './mail.png'
+import pin from './pin.png'
 import phone from './phone.png'
 import plus from './plus.png'
+import plusBlack from './plus-black.png'
 import profile from './profile.png'
 import rocket from './rocket.png'
 import upload from './upload.png'
 
 export {
+    accordionDown,
+    accordionUp,
+    bin,
     calendar,
     chain,
     clock,
+    edit,
+    file,
     flag,
     home,
     key,
     mail,
+    pin,
     phone,
     plus,
+    plusBlack,
     profile,
     rocket,
     upload
