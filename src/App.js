643d490d21b1a88d26cc3069222cd8cb69df853c
Phung Nguyen <44967773+PhungNg@users.noreply.github.com>
2022-11-01 16:10:07 +0100

    Create candidate profile

--- src/App.js
+++ src/App.js
@@ -2,64 +2,97 @@ import React, { useState, useEffect } from 'react';
 import Button from './Components/Button';
 import Table from './Components/Table';
 import SearchIcon from './Icons/search-icon.js';
-import { db, getCandidates } from './Firebase/firebaseConfig';
-// import { collection, getDocs } from "firebase/firestore";
+import { db, getCandidates, storage } from './Firebase/firebaseConfig';
+import { listAll, ref, getDownloadURL } from "firebase/storage";
 import NewCandidateForm from './newCandidateForm';
+import CandidateProfile from './candidateProfile';
 import './App.scss';
+import './utilities.scss';
 
 function App() {
-  const [dialog, setDialog] = useState(true)
-  const [candidates, setCandidates] = useState([])
+  const [ addNewCandidate, setAddNewCandidate ] = useState(false)
+  const [ candidateProfile, setCandidateProfile ] = useState(false)
+  const [ candidates, setCandidates ] = useState([])
+  const [ fileList, setFileList ] = useState([])
 
   const fetchData = async () => {
     const response = await getCandidates()
     setCandidates(candidates => candidates = response)
   }
-  
-  const Candidate = () => {
-    return (candidates.map(({firstname, lastname}) =>
-      <div>
-        <p>{firstname} {lastname}</p>
-      </div>  
-    )) 
+
+  const Filter = () => {
+
+    const filters = ["Alle kandidater", "Arkitektur", "Experiencer", "Test og prosjekt", "Utvikling"]
+
+    return (
+      <div class="filter">
+
+      </div>
+    )
   }
+
   useEffect(()=> {
     fetchData()
+    // const fileListRef = ref(storage, "files/")
+    // listAll(fileListRef).then((response) => {
+    //   response.items.forEach(item => {
+    //     getDownloadURL(item).then((url) => {
+    //       setFileList(prev => [...prev, url])
+    //     })
+    //   })
+    // })
   },[])
 
+  const getCandidate = (data) => {
+    setCandidateProfile(data)
+  }
+
   return (
-    <div className="App m-auto">
+    <div className="App">
       <header>
         <h1>Kandidater</h1>
-        <Button text="Legg til ny kandidat +" selected callback={() => setDialog(dialog => dialog = true)}/>
+        <Button text="Legg til ny kandidat" icon="plus" className={"pc-400"} onClick={() => setAddNewCandidate(true)}/>
       </header>
+
       <main>
         <div className="filter">
-          <Button text="Alle kandidater" selected/>
+          <Button text="Alle kandidater" className="pc-400" />
           <Button text="Arkitektur" amount="2"/>
           <Button text="Experience"/>
           <Button text="Test og prosjekt"/>
           <Button text="Utvikling"/>
-          <Candidate />
         </div>
+
         <section className="candidates">
           <div className="search-box">
             <SearchIcon />
             <input className='search-input' type="text" placeholder="søk" />
           </div>
-          <Table headers={["Navn", "Stilling", "Oppfølging", "Ansvarlig", "Telefon", "Status"]}/>
+          <Table candidates={candidates} onClick={getCandidate}/>
         </section>
         
-        {dialog &&
-          <div className="dialog">
-            <section>
-              <div className="dialog-body">
-                {/* <button type="button" onClick={() => setDialog(!dialog)}>X</button> */}
-                <NewCandidateForm cancel={() => setDialog(!dialog)}/>
-              </div>
-            </section>
-          </div>
-        }
+        <div className={`dialog${candidateProfile || addNewCandidate ? " open" : ""}`}>
+          <section>
+            <header className="dialog-header">
+              <h1>
+                {candidateProfile
+                  ? `${candidateProfile.firstname} ${candidateProfile.lastname}`
+                  : "Legg til ny kandidat"
+                }
+              </h1>
+              <button className="btn-close"
+                onClick={()=>{
+                  setAddNewCandidate(false)
+                  setCandidateProfile(false)}}>
+                    X
+              </button>
+            </header>
+            <div className="dialog-body">
+              {addNewCandidate && <NewCandidateForm />}
+              {candidateProfile && <CandidateProfile candidate={candidateProfile}/>}
+            </div>
+          </section>
+        </div>
       </main>
     </div>
   );
