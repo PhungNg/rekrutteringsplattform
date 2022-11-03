376cc397bd67884e0a22bcb58b4d6d87b0c9c4bd
Phung Nguyen <44967773+PhungNg@users.noreply.github.com>
2022-11-03 09:09:55 +0100

    Update newCandidateForm.js

--- src/newCandidateForm.js
+++ src/newCandidateForm.js
@@ -1,44 +1,51 @@
 import React, { useState, useRef, useEffect } from 'react'
 import {addCandidate} from './Firebase/firebaseConfig'
 import { storage } from './Firebase/firebaseConfig'
-import { ref, uploadBytes } from 'firebase/storage'
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
 import Input from './Components/Input'
 
 const NewCandidateForm = () => {
-    const [ firstStep, setFirstStep ] = useState(true)
-    const [ formObject, setFormObject ] = useState()
-    const [ filesUpload, setFilesUpload ] = useState()
-    const [ formComplete, setFormComplete ] = useState(false)
-
-    const formRef = useRef(null)
-
-    const addNewUser = () => {
-      addCandidate(formObject)
-      uploadFiles()
-    }
-    
-    const uploadFiles = () => {
-      for(let file in filesUpload) {
-        const filesRef = ref(storage, `${formObject.firstname}`)
-
-        uploadBytes(filesRef, uploadFiles)
+  const [ firstStep, setFirstStep ] = useState(true)
+  const [ formObject, setFormObject ] = useState()
+  const [ filesObject, setFilesObject ] = useState([])
+  const [formComplete, setFormComplete ] = useState(false)
+  const [fileCount, setFileCount ] = useState(0)
+  const formRef = useRef(null)
+  
+  const uploadFiles = (files) => {
+    for(let key in files) {
+      const fileRef = ref(storage, `${formObject.firstname}${formObject.lastname}/${files[key].name}`)
+      if(files[key].name !== '') {
+        uploadBytes(fileRef, files[key])
+        .then(response => {
+          getDownloadURL(fileRef)
+            .then((url) => {
+              let obj = {
+                [key]: {
+                  fileName: files[key].name,
+                    url: url
+                  }
+                }
+              setFilesObject( filesObject => ([...filesObject, obj]))
+              setFileCount(fileCount => fileCount += 1)
+            })
+        })
       }
     }
+  }
+  const formHandler = (e) => {
+    e.preventDefault()
+    let data = new FormData(formRef.current);
+    if(!formObject){
+      setFormObject(Object.fromEntries(data.entries()))
+      setFirstStep(!firstStep)
+    } else {      
+      uploadFiles(Object.fromEntries(data.entries()))
+      setFormComplete(true)
+    }
+  }
 
-    const formHandler = (e) => {
-        e.preventDefault()
-        let data = new FormData(formRef.current);
-        
-        if(firstStep){
-          setFormObject(Object.fromEntries(data.entries()))
-          setFirstStep(!firstStep)
-        } else {
-          setFilesUpload(Object.fromEntries(data.entries()))
-          setFormComplete(true)
-        }
-      }
-
-    const BasicInfo = () => (
+  const BasicInfo = () => (
       <fieldset form="new-candidate" className="basic-info">
         <Input id="firstname" placeholder="Ola" label="Fornavn"/>
         <Input id="lastname" placeholder="Nordmann" label="Etternavn"/>
@@ -67,36 +74,47 @@ const NewCandidateForm = () => {
         <Input id="followUpTime" type="date" label="Oppfølgings tid"/>
         <Input id="comments" textarea rows={4} placeholder="Litt om kandidaten" label="Kommentar" />
       </fieldset>
-    )
-
-    const FileUpload = () => {
-      return (
-        <fieldset form="new-candidate" className="file-upload">
-          <Input type="file" id="cv" label="Last opp CV"/>
-          <Input type="file" id="grades" label="Last opp karakterutskrift studie"/>
-          <Input type="file" id="gradesVgs" label="Last opp karakterutskrift VGS"/>
-        </fieldset>
-      )
-    }
-
-    useEffect(() => {
-      if(formComplete && filesUpload) {
-        addNewUser()
-      }
-    })
-
+  )
+  
+  const FileUpload = () => {
     return (
-        <form id="new-candidate" ref={formRef}>
-          { firstStep
-              ? <BasicInfo />
-              : <FileUpload />
-          }
-          <div className="button-container">
-            {!firstStep && <button className="btn" onClick={()=> setFirstStep(!firstStep)}>Forrige</button>}
-            <button className="btn pc-400" form="new-candidate" onClick={(e)=> formHandler(e)}>{formObject ? "Lagre" : "Neste"}</button>
-          </div>
-        </form>
+      <fieldset form="new-candidate" className="file-upload">
+        <Input type="file" id="applicationText" label="Søknadstekst"/>
+        <Input type="file" id="cv" label="Last opp CV"/>
+        <Input type="file" id="grades" label="Last opp karakterutskrift studie"/>
+        <Input type="file" id="gradesVgs" label="Last opp karakterutskrift VGS"/>
+      </fieldset>
     )
+  }
+  
+  useEffect(() => {
+    console.log("fObj", formObject)
+    if(fileCount === 4) {
+      console.log(filesObject)
+      setFormObject(formObject => ({...formObject, ...{files: filesObject}}))
+      setFormComplete(true)
+    }
+  },[fileCount])
+  
+  useEffect(() => {
+    if(formComplete) {
+      console.log("Rett før",formObject)
+      addCandidate(formObject)
+    }
+  },[formComplete, formObject])
+  
+  return (
+      <form id="new-candidate" ref={formRef}>
+        { firstStep
+            ? <BasicInfo />
+            : <FileUpload />
+        }
+        <div className="button-container">
+          {!firstStep && <button className="btn" onClick={()=> setFirstStep(!firstStep)}>Forrige</button>}
+          <button className="btn pc-400" form="new-candidate" onClick={(e)=> formHandler(e)}>{formObject ? "Lagre" : "Neste"}</button>
+        </div>
+      </form>
+  )
 }
 
 export default NewCandidateForm;
