b7fd4f0fac1c8bf2ac1b5683204d89193da89237
Phung Nguyen <44967773+PhungNg@users.noreply.github.com>
2022-11-08 15:48:10 +0100

    Update Dialog and Filter function

--- src/App.js
+++ src/App.js
@@ -1,98 +1,128 @@
-import React, { useState, useEffect } from 'react';
-import Button from './Components/Button';
-import Table from './Components/Table';
-import SearchIcon from './Icons/search-icon.js';
-import { db, getCandidates, storage } from './Firebase/firebaseConfig';
-import { listAll, ref, getDownloadURL } from "firebase/storage";
-import NewCandidateForm from './newCandidateForm';
-import CandidateProfile from './candidateProfile';
+import React, { useState, useEffect, useCallback } from 'react';
+import {
+  Button,
+  CandidateProfile ,
+  Dialog as DialogComp,
+  Input,
+  NewCandidateForm,
+  Table
+} from './Components/';
+import { funnel, search } from './Icons';
+import { getCandidates, filter } from './Firebase/firebaseConfig';
 import './App.scss';
 import './utilities.scss';
+import { plus } from './Icons';
 
 function App() {
-  const [ addNewCandidate, setAddNewCandidate ] = useState(false)
   const [ candidateProfile, setCandidateProfile ] = useState(false)
   const [ candidates, setCandidates ] = useState([])
-  const [ fileList, setFileList ] = useState([])
-
-  const fetchData = async () => {
+  const [ openDialog, setOpenDialog ] = useState()
+  const [ currentSelected, setCurrentSelected ] = useState("Alle kandidater")
+  
+  const fetchData = useCallback(async () => {
     const response = await getCandidates()
     setCandidates(candidates => candidates = response)
+  },[])
+  
+  const handelCloseDialog = useCallback((bool) => {
+    fetchData()
+    setOpenDialog(null)
+    return bool
+  },[fetchData])
+
+  useEffect(()=>{
+    fetchData()
+  },[fetchData])
+
+  const handelOpenDialog = (target, data) => {
+    setOpenDialog(target)
+    if(data) setCandidateProfile(data)
   }
 
-  const Filter = () => {
+  const Dialog = () => {
+    const [ formStep, setFormStep ] = useState(1)
 
-    const filters = ["Alle kandidater", "Arkitektur", "Experiencer", "Test og prosjekt", "Utvikling"]
+    const handelFormStep = (bool) => {
+      if(bool) {
+        setFormStep(formStep => formStep += 1)
+      } else {
+        setFormStep(formStep => formStep -= 1)
+      }
+    }
+
+    let attrs = {
+      openDialog: openDialog,
+      closeDialog: handelCloseDialog,
+    }
+
+    if(openDialog === "profile") {
+      attrs = {...attrs,
+        header: `${candidateProfile.firstname} ${candidateProfile.lastname}`,
+        content: <CandidateProfile candidate={candidateProfile} closeDialog={handelCloseDialog}/>
+      }
+    }
+    
+    if(openDialog === "form"){
+      attrs = {...attrs,
+        header: formStep < 3 ? `Legg til ny kandidata (${formStep}/2)` : null,
+        formStep: formStep,
+        content: <NewCandidateForm closeDialog={handelCloseDialog} setFormStep={handelFormStep} formStep={formStep}/>
+      }      
+    }
+
+    return (<DialogComp {...attrs}/>)
+  }
+  
+  const Filter = () => {
+    const filterBtns = ["Alle kandidater", "Arkitektur", "Experience", "Test og prosjekt", "Utvikling"]
+
+    const handelFilter = async (field, value) => {
+      setCurrentSelected(value)
+      
+      if(value === "Alle kandidater") return fetchData()
+      setCandidates(await filter(field, value))
+    }
 
     return (
-      <div class="filter">
-
+      <div className="filter">
+        {filterBtns.map((value, key) => (
+            <Button key={key} text={value} onClick={()=>handelFilter("department", value)} className={currentSelected === value ? "pc-400" : ""}/>
+        ))}
       </div>
     )
   }
 
-  useEffect(()=> {
-    fetchData()
-    // const fileListRef = ref(storage, "files/")
-    // listAll(fileListRef).then((response) => {
-    //   response.items.forEach(item => {
-    //     getDownloadURL(item).then((url) => {
-    //       setFileList(prev => [...prev, url])
-    //     })
-    //   })
-    // })
-  },[])
-
-  const getCandidate = (data) => {
-    setCandidateProfile(data)
+  const handelSort = (column) => {
+    const compare = (a,b) => {
+      for (const key in a) {
+        if([key][0] === column) {
+          if (a[key] < b[key]){
+            return -1;
+          }
+          if (a[key] > b[key]){
+            return 1;
+          }
+          return 0;
+        }
+      }
+    }
+    setCandidates([...candidates.sort(compare)] )
   }
 
   return (
     <div className="App">
       <header>
         <h1>Kandidater</h1>
-        <Button text="Legg til ny kandidat" icon="plus" className={"pc-400"} onClick={() => setAddNewCandidate(true)}/>
+        <Button text="Legg til ny kandidat" icon={plus} className={"pc-400"} onClick={() => handelOpenDialog("form")}/>
       </header>
-
       <main>
-        <div className="filter">
-          <Button text="Alle kandidater" className="pc-400" />
-          <Button text="Arkitektur" amount="2"/>
-          <Button text="Experience"/>
-          <Button text="Test og prosjekt"/>
-          <Button text="Utvikling"/>
-        </div>
-
+        <Filter />
         <section className="candidates">
-          <div className="search-box">
-            <SearchIcon />
-            <input className='search-input' type="text" placeholder="søk" />
-          </div>
-          <Table candidates={candidates} onClick={getCandidate}/>
+            <Input placeholder="Søk" icon={search}/>
+          <Table candidates={candidates} onClick={handelOpenDialog} handelSort={handelSort}/>
         </section>
-        
-        <div className={`dialog${candidateProfile || addNewCandidate ? " open" : ""}`}>
-          <section>
-            <header className="dialog-header">
-              <h1>
-                {candidateProfile
-                  ? `${candidateProfile.firstname} ${candidateProfile.lastname}`
-                  : "Legg til ny kandidat"
-                }
-              </h1>
-              <button className="btn-close"
-                onClick={()=>{
-                  setAddNewCandidate(false)
-                  setCandidateProfile(false)}}>
-                    X
-              </button>
-            </header>
-            <div className="dialog-body">
-              {addNewCandidate && <NewCandidateForm />}
-              {candidateProfile && <CandidateProfile candidate={candidateProfile}/>}
-            </div>
-          </section>
-        </div>
+
+        <Dialog />
       </main>
     </div>
   );
