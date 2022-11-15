3e43bb011fad8914e8cce5f453fe559decd45521
Phung Nguyen <44967773+PhungNg@users.noreply.github.com>
2022-11-15 07:43:35 +0100

    Add click outside handler

--- src/Components/Dropdown/index.js
+++ src/Components/Dropdown/index.js
@@ -1,18 +1,30 @@
 import React, { useState, useEffect } from 'react'
-import { getCandidates,} from '../../Firebase/firebaseConfig';
 
 
 import { Button, Input } from '../index'
 import { funnel } from '../../Icons'
 import './index.scss'
+import { useRef } from 'react';
 
-const Dropdown = ({checkboxList, handelOnChange}) => {
-  const [ isOpen, setIsOpen ] = useState(true)
-  // const [ checkboxes, setCheckboxes ] = useState([])
+const Dropdown = ({checkboxList, handelOnChange, handelOpenClose, isOpen}) => {
+  const contentRef = useRef(null)
+ 
+  useEffect(() => {
+    const handelClickOutside = (e) => {
+      if(contentRef.current && !contentRef.current.contains(e.target)) {
+        handelOpenClose()
+      }
+    }
+    if(isOpen) {
+      window.addEventListener('mouseup', handelClickOutside)
+    }
+    return () => window.removeEventListener('mouseup', handelClickOutside)
+  },[handelOpenClose, isOpen])
+
   return (
     <div className="dropdown">
-      <Button text="Filter" icon={funnel} className="dropdown-btn" onClick={()=>setIsOpen(!isOpen)}/>
-      <div className={`content${isOpen ? " open" : ""}`}>
+      <Button text="Filter" icon={funnel} className="dropdown-btn" onClick={handelOpenClose}/>
+      <div ref={contentRef} className={`content${isOpen ? " open" : ""}`}>
         <section className="header d-flex justify-content-between align-items-center">
           <h5>Ansvarlig</h5>
           <Button text="Fjern alle" className="clear-filter-btn"/>
